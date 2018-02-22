0554dffa39e8bd7dfd7bf8fd7e8fe9c8d957fdc2
Leo Knudsen <nightskater-220@hotmail.com>
2018-02-22 09:36:17 +0100

    added company to seeder

--- database/seeds/CompaniesTableSeeder.php
+++ database/seeds/CompaniesTableSeeder.php
@@ -13,6 +13,19 @@ class CompaniesTableSeeder extends Seeder
      */
     public function run()
     {
+
+    	Company::create([
+    		'user_id' => 1,
+    		'company_name' => 'Knudsen Design & Udvikling',
+    		'company_type' => 'Enkeltmand',
+    		'company_address' => 'Guldbergsgade 113 4TV',
+    		'company_registration_nr' => str_random(8),
+    		'company_phone_nr' => '+45 27 45 94',
+    		'company_bank_account_nr' => 'xxxx xxxx xxxx xxxx',
+    		'company_iban_nr' => "null",
+    		'company_swift_address' => "null"
+    	]);
+
         factory(Company::class, rand(1, 10))->create();
     }
 }
